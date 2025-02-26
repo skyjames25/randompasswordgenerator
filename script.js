eccdfb114788844159a0b7bf167b7f7d00f3497a
jameslim <skyjames25@gmail.com>
2025-02-26 22:34:30 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+let passwordBox1 = document.querySelector(".password-box1")
+let passwordBox2 = document.querySelector(".password-box2")
+
+
+
+
+
+
+function generatePasswords() {
+    let passwordsList = []
+
+    for (let i=0;i<2;i++){
+        randomPasswords = ""
+        for (let i=0; i<15; i++){
+            let chr = characters[Math.floor(Math.random() * characters.length)]
+            randomPasswords +=chr
+        }
+        passwordsList.push(randomPasswords)
+    }
+    passwordBox1.textContent = passwordsList[0]
+    passwordBox2.textContent = passwordsList[1]
+}
